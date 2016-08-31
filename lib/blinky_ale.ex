1296202c60e341f5f8ee9ed3d46ebc9a1a47748c
Dmitry Harnitski <dmitry@brainjocks.com>
2016-08-31 11:53:10 -0400

    make it blink

--- lib/blinky_ale.ex
+++ lib/blinky_ale.ex
@@ -17,4 +17,18 @@ defmodule BlinkyAle do
     Supervisor.start_link(children, opts)
   end
 
+  def blink do
+    :os.cmd '/usr/bin/pinmux set ephy gpio'
+    {:ok, pid} = Gpio.start_link(43, :output)
+    blink_forever(pid)
+  end
+
+  def blink_forever(pid) do
+    Gpio.write(pid, 1)
+    :timer.sleep(1000)
+    Gpio.write(pid, 0)
+    :timer.sleep(1000)
+    blink_forever(pid)
+  end
+
 end
